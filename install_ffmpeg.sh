d692fb8991ad87fbe007176abc3ede2a5bd2e092
Ulrich Pech <mixmasteru@users.noreply.github.com>
2017-08-27 11:25:20 +0200

    renew

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Based on instructions found here: http://wiki.razuna.com/display/ecp/FFMpeg+Installation+on+CentOS+and+RedHat#FFMpegInstallationonCentOSandRedHat-InstallX264
-
+# Based on gist.github.com/gboudreau/install-ffmpeg-amazon-linux.sh
+# and https://trac.ffmpeg.org/wiki/CompilationGuide/Centos
 if [ "`/usr/bin/whoami`" != "root" ]; then
     echo "You need to execute this script as root."
     exit 1
@@ -16,9 +16,10 @@ gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6
 enabled=1
 priority=1
 protect=1
-includepkgs=SDL SDL-devel gsm gsm-devel libtheora theora-tools libdc1394 libdc1394-devel libraw1394-devel
+includepkgs=SDL SDL-devel gsm gsm-devel libtheora theora-tools
 EOF
 rpm --import http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6
+rpm -Uhv http://ec2-23-22-86-129.compute-1.amazonaws.com/pub/sam/1.3/el6/x86_64/SAM_brew_latest/toplink/packages/libraw1394/2.0.4/1.el6/x86_64/libraw1394-2.0.4-1.el6.x86_64.rpm
 
 rpm -Uhv ftp://195.220.108.108/linux/dag/redhat/el6/en/x86_64/dag/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 yum -y update
@@ -29,8 +30,6 @@ yum -y install freetype-devel giflib gsm gsm-devel imlib2 imlib2-devel lame lame
 yum -y install libXau-devel libXdmcp-devel libXext-devel libXrandr-devel libXrender-devel libXt-devel
 yum -y install libogg libvorbis vorbis-tools mesa-libGL-devel mesa-libGLU-devel xorg-x11-proto-devel zlib-devel
 yum -y install libtheora theora-tools
-yum -y install openssl
-yum -y install openssl-devel
 yum -y install ncurses-devel
 yum -y install libdc1394 libdc1394-devel
 yum -y install amrnb-devel amrwb-devel opencore-amr-devel
@@ -60,10 +59,12 @@ cd libtheora-1.1.1
 ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-examples --disable-shared --disable-sdltest --disable-vorbistest && make && make install
 
 cd /opt
-wget http://downloads.sourceforge.net/opencore-amr/vo-aacenc-0.1.2.tar.gz
-tar xzvf vo-aacenc-0.1.2.tar.gz && rm -f vo-aacenc-0.1.2.tar.gz
-cd vo-aacenc-0.1.2
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared && make install
+git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
+cd fdk-aac
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
 
 cd /opt
 curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
@@ -88,7 +89,7 @@ git checkout tags/v1.3.0
 cd /opt
 git clone git://git.videolan.org/x264.git
 cd x264
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-asm --enable-static && make install
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-asm && make install
 
 export LD_LIBRARY_PATH=/usr/local/lib/:$HOME/ffmpeg_build/lib/
 echo /usr/local/lib >> /etc/ld.so.conf.d/custom-libs.conf
@@ -98,27 +99,22 @@ ldconfig
 cd /opt
 git clone git://source.ffmpeg.org/ffmpeg.git
 cd ffmpeg
-
-#./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --#bindir="$HOME/bin" \
-#--extra-libs=-ldl --enable-version3 --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libvpx --enable-libfaac \
-#--enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libvo-aacenc --enable-libxvid --disable-ffplay \
-#--enable-gpl --enable-postproc --enable-nonfree --enable-avfilter --enable-pthreads --arch=x86_64 --enable-openssl && make install
-
-# 2.2
-# git checkout release/2.2
-# PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
-# export PKG_CONFIG_PATH
-# ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs=-ldl --enable-version3 --enable-libvpx --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libvo-aacenc --enable-libxvid --disable-ffplay --enable-gpl --enable-postproc --enable-nonfree --enable-avfilter --enable-pthreads --arch=x86_64 --enable-openssl
-# make install
-
-# 3.3.3
-git checkout n3.3.3
+git checkout release/3.0
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
 export PKG_CONFIG_PATH
-./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-cflags="-fPIC" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs=-ldl --enable-version3 --enable-libvpx --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --disable-ffplay --enable-gpl --enable-postproc --enable-nonfree --enable-avfilter --enable-pthreads --arch=x86_64 --enable-openssl
-make install
+./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" \
+--extra-libs=-ldl --enable-version3 --enable-libvpx \
+--enable-libx264 --enable-libmp3lame --enable-libtheora --enable-libvorbis  --enable-libxvid --disable-ffplay \
+--enable-gpl --enable-postproc --enable-nonfree --enable-avfilter --enable-pthreads --arch=x86_64 && make install
 
 # Test the resulting ffmpeg binary
 cp $HOME/bin/ffmpeg /usr/bin/
-ffmpeg -version
+cp $HOME/bin/ffprobe /usr/bin/
+cp $HOME/bin/ffserver /usr/bin/
+cp $HOME/bin/vsyasm /usr/bin/
+cp $HOME/bin/x264 /usr/bin/
+cp $HOME/bin/yasm /usr/bin/
+cp $HOME/bin/ytasm /usr/bin/
 
+
+ffmpeg -version

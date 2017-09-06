6f692ae9bfb4cf94b22ee6928abdc67093522330
Ulrich Pech <mixmasteru@users.noreply.github.com>
2017-09-06 22:36:16 +0200

    add pyenv

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -28,10 +28,19 @@ unzip opencv.zip
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.3.0.zip
 unzip opencv_contrib.zip
 
-pip3 install --upgrade pip
-pip3 install virtualenv
-virtualenv -p /usr/bin/python3 venv
-source venv/bin/activate
+#pyenv
+apt-get install -y build-essential libbz2-dev libssl-dev libreadline-dev libsqlite3-dev tk-dev
+# optional scientific package headers (for Numpy, Matplotlib, SciPy, etc.)
+apt-get install -y libpng-dev libfreetype6-dev
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+pyenv install 3.6.1
+pyenv global 3.6.1
 pip install numpy
 #pip install gphoto2
 
@@ -39,19 +48,24 @@ cd opencv-3.3.0
 mkdir build
 cd build
 
-# hack to force python3:
-mv /usr/bin/python2.7 /usr/bin/back_python2.7
-
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D INSTALL_C_EXAMPLES=OFF \
-    -D OPENCV_EXTRA_MODULES_PATH=/root/opencv_contrib-3.3.0/modules \
-    -D PYTHON_EXECUTABLE=/root/venv/bin/python3 \
-    -D PYTHON_DEFAULT_EXECUTABLE=/root/venv/bin/python3 \
-    -D BUILD_opencv_python3=ON \
-    -D BUILD_EXAMPLES=ON ..
-
+-D CMAKE_INSTALL_PREFIX=~/.pyenv/versions/3.6.1/usr/local/ \
+-D INSTALL_C_EXAMPLES=OFF \
+-D BUILD_NEW_PYTHON_SUPPORT=ON \
+-D BUILD_opencv_python3=ON \
+-D BUILD_opencv_legacy=OFF \
+-D INSTALL_PYTHON_EXAMPLES=ON \
+-D OPENCV_EXTRA_MODULES_PATH=/root/opencv_contrib-3.3.0/modules \
+-D BUILD_EXAMPLES=ON \
+-D PYTHON_EXECUTABLE=~/.pyenv/versions/3.6.1/bin/python \
+-D PYTHON_LIBRARY=~/.pyenv/versions/3.6.1/lib/libpython3.6m.a \
+-D PYTHON_INCLUDE_DIR=~/.pyenv/versions/3.6.1/include/python3.6m \
+-D PYTHON_INCLUDE_DIRS=~/.pyenv/versions/3.6.1/include/python3.6m \
+-D PYTHON_INCLUDE_DIRS2=~/.pyenv/versions/3.6.1/include/python3.6m \
+-D INCLUDE_DIRS=~/.pyenv/versions/3.6.1/include/python3.6m \
+-D INCLUDE_DIRS2=~/.pyenv/versions/3.6.1/include/python3.6m \
+-D PYTHON_PACKAGES_PATH=~/.pyenv/versions/3.6.1/lib/python3.6/site-packages \
+-D PYTHON_NUMPY_INCLUDE_DIR=~/.pyenv/versions/3.6.1/lib/python3.6/site-packages/numpy/core/include ..
 
 make -j 3 install
 ldconfig

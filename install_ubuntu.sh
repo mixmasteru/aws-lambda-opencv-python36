743be375bc9fc77ff800f5091448f3c8c954c330
Ulrich Pech <mixmasteru@users.noreply.github.com>
2017-09-06 21:10:39 +0200

    small fixes

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -11,6 +11,7 @@ apt-get upgrade -y
 
 apt-get install -y python3.6
 apt-get install -y python3-pip
+apt-get install -y python3-dev
 apt-get install -y ffmpeg
 apt-get install unzip
 
@@ -19,8 +20,6 @@ libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscal
 libavdevice-dev libgtk-3-dev libavutil-dev libswscale-dev libavresample-dev libavfilter-dev libgphoto2-dev gfortran
 
 cd ~
-mkdir opencv
-cd opencv
 # source: http://opencv.org/releases.html
 wget -O opencv.zip https://github.com/opencv/opencv/archive/3.3.0.zip
 unzip opencv.zip
@@ -47,15 +46,17 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D INSTALL_C_EXAMPLES=OFF \
-    -D OPENCV_EXTRA_MODULES_PATH=/root/opencv/opencv_contrib-3.3.0/modules \
-    -D PYTHON_EXECUTABLE=/root/python3/bin/python \
+    -D OPENCV_EXTRA_MODULES_PATH=/root/opencv_contrib-3.3.0/modules \
+    -D PYTHON_EXECUTABLE=/root/venv/bin/python3 \
+    -D PYTHON_DEFAULT_EXECUTABLE=/root/venv/bin/python3 \
+    -D BUILD_opencv_python3=ON \
     -D BUILD_EXAMPLES=ON ..
 
 
 make -j 3 install
 ldconfig
 
-cd ~/opencv/python3/lib/python3.6/site-packages/
+cd ~/venv/lib/python3.6/site-packages/
 ln -s /usr/local/lib/python3.6/site-packages/cv2.cpython-36m-x86_64-linux-gnu.so cv2.so
 cd ~/opencv
 
